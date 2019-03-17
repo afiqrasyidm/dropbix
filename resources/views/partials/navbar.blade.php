712a22c130ae5f66716db5f389a5932b5cf11f74
afiqrasyidm <afiqrasyidm@gmail.com>
2019-03-17 12:53:57 +0700

    add guard

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,19 +5,21 @@
             </div>
             <ul class="nav navbar-nav">
                <li class="active"><a href="{{ route('home') }}">Home</a></li>
-			   <li class="active"><a href="{{ route('upload') }}">Upload File</a></li>
-			   <li class="active"><a href="{{ route('list-file') }}">List File</a></li>
-            </ul>
-			
-			 <ul class="nav navbar-nav navbar-right">
-            @if(Auth::user())
-				
 
-			    <li><a href="{{ route('logout') }}"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>    
-			@else
-		       <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
-               <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>      
-			@endif
+                @if(Auth::user())
+                  <li class="active"><a href="{{ route('upload') }}">Upload File</a></li>
+			             <li class="active"><a href="{{ route('list-file') }}">List File</a></li>
+                @endif
+
+            </ul>
+
+			 <ul class="nav navbar-nav navbar-right">
+             @if(Auth::user())
+			         <li><a href="{{ route('logout') }}"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+			       @else
+		           <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
+               <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			       @endif
             </ul>
          </div>
-      </nav>  
+      </nav>
